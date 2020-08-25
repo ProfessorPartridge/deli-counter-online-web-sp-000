30385737f1e6b5765941ab83fdbe47e7cc79bd01
Josiah  Partridge <jpartridj94@gmail.com>
2020-08-25 04:43:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+katz_deli = []
+
+def line(array_line)
+  hash = Hash.new
+  if array_line.empty? == FALSE
+    array_line.each_with_index { |customer, position|
+      hash[customer] = position
+    }
+    puts "The line is currently: #{}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(array, new_customer)
+  
+end
+
+def now_serving(array)
+  
+end
